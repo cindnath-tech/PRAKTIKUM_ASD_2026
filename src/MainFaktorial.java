3b330a93df388d6231f77e9759edd1df8cf4151f
cindnath-tech <cindnathania@gmail.com>
2026-03-12 11:11:03 +0700

    Percobaan 1

---
+++ src/MainFaktorial.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class MainFaktorial {
+    
+    public static void main(String[] args) {
+        Scanner cindy = new Scanner(System.in);
+        System.out.print("Masukkan nilai : ");
+        int nilai = cindy.nextInt();
+
+        Faktorial fk = new Faktorial();
+        System.out.println("Nilai Faktorial " + nilai + " menggunakan BF : " +fk.faktorialBF(nilai));
+        System.out.println("Nilai faktorial " + nilai + " menggunakan DC : " +fk.faktorialDC(nilai));
+    }
+}
