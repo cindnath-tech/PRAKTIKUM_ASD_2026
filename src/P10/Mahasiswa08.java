dfde14a547349ddb4e0820c0af07848f53ba6a48
cindnath-tech <cindnathania@gmail.com>
2026-05-10 11:38:15 +0700

    Percobaan 2

---
+++ src/P10/Mahasiswa08.java
@@ -0,0 +1,19 @@
+package P10;
+
+public class Mahasiswa08 {
+    String nim;
+    String nama;
+    String prodi;
+    String kelas;
+
+    Mahasiswa08(String nim, String nama, String prodi, String kelas) {
+        this.nim = nim;
+        this.nama = nama;
+        this.prodi = prodi;
+        this.kelas = kelas;
+    }
+
+    public void tampilkanData() {
+        System.out.println(nim + " - " + nama + " - " + prodi + " - " + kelas);
+    }
+}
